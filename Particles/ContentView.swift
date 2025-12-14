42368ac9c9290d2701b3bd643420454a72a23cdd
developer-3108 <developer.akshat310804@gmail.com>
2025-12-14 23:22:23 +0530

    Initial Commit

---
+++ Particles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Particles
+//
+//  Created by Akshat Srivastava on 14/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Particles/ParticlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParticlesApp.swift
+//  Particles
+//
+//  Created by Akshat Srivastava on 14/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ParticlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
